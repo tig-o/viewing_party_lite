8df9305f28f0db8c4a3f074a81bb3b7c972db28c
Thiago Silveira <thiago1992@yahoo.com>
2022-09-06 16:32:51 -0600

    User Story 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,17 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    user = User.create!(user_params)
-    redirect_to user_path(user.id)
+    user = User.new(user_params)
+    if user.save
+      redirect_to "/users/#{user.id}"
+      flash[:success] = "Welcome, #{user.name}!"
+    else
+      flash[:alert] = "Error: #{error_message(user.errors)}"
+      redirect_to '/register'
+    end
   end
 
   def user_params
-    params.permit(:name, :email)
+    params.permit(:name, :email, :password)
   end
 end
